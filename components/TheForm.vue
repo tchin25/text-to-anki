e9c8c9659c43172a4a22c965cd4737fadb564a1a
Thomas Chin <tchin25@gmail.com>
2020-07-15 21:36:42 -0400

    added scss and styled form

--- components/TheForm.vue
+++ components/TheForm.vue
@@ -1,45 +1,51 @@
 <template>
   <section>
-    <div class="field">
-      <label class="label">Card Model</label>
-      <div class="control">
-        <div class="select">
-          <select v-model="selectModels">
-            <option disabled value="">Please select one</option>
-            <option v-for="(model, key) in selectModels" :key="key">{{
-              model
-            }}</option>
-          </select>
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <div class="field">
+          <label class="label">Card Model</label>
+          <div class="control">
+            <div class="select form-input">
+              <select v-model="selectModels">
+                <option disabled value="">Please select one</option>
+                <option v-for="(model, key) in selectModels" :key="key">{{
+                  model
+                }}</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Deck</label>
+          <div class="control">
+            <div class="select form-input">
+              <select v-model="selectDecks">
+                <option disabled value="">Please select one</option>
+                <option v-for="(deck, key) in selectDecks" :key="key">{{
+                  deck
+                }}</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Tokenizer Characters</label>
+          <div class="control">
+            <input class="input form-input" type="text" :placeholder="regex" />
+          </div>
+          <p class="help">Regex characters. Leave blank for default.</p>
+        </div>
+        <div class="control">
+          <button class="button is-primary form-input">Submit</button>
         </div>
       </div>
-    </div>
-    <div class="field">
-      <label class="label">Deck</label>
-      <div class="control">
-        <div class="select">
-          <select v-model="selectDecks">
-            <option disabled value="">Please select one</option>
-            <option v-for="(deck, key) in selectDecks" :key="key">{{
-              deck
-            }}</option>
-          </select>
+      <div class="column">
+        <div class="field">
+          <label class="label">Paste Text Here</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+          </div>
         </div>
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">Tokenizer Characters</label>
-      <div class="control">
-        <input class="input" type="text" :placeholder="regex" />
-      </div>
-      <p class="help">Regex characters. Leave blank for default.</p>
-    </div>
-    <div class="control">
-      <button class="button is-primary">Submit</button>
-    </div>
-    <div class="field">
-      <label class="label">Paste Text Here</label>
-      <div class="control">
-        <textarea class="textarea" placeholder="Textarea"></textarea>
       </div>
     </div>
   </section>
@@ -49,8 +55,7 @@
 export default {
   data() {
     return {
-      regex:
-        "。…’‘、「」【】『』!\"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^\n_`{|}~"
+      regex: "。…’‘、「」【】『』!\"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^\n_`{|}~"
     };
   },
   computed: {
@@ -74,4 +79,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.form-input {
+    width: 100%;
+    select {
+        width: 100%;
+    }
+}
+
+</style>
