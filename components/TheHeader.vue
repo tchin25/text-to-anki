7fb8f48889bc699fb7b86d1aa8fb77e4cbdaf307
Thomas Chin <tchin25@gmail.com>
2020-07-16 15:20:04 -0400

    added header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,45 @@
 <template>
-<navbar>
-</navbar>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a
+        role="button"
+        class="navbar-burger burger"
+        :class="isActive ? 'is-active' : ''"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarItems"
+        @click="isActive = !isActive"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarItems" class="navbar-menu" :class="isActive ? 'is-active' : ''">
+      <div class="navbar-start ml-6">
+        <nuxt-link to="/" class="navbar-item">
+          Home
+        </nuxt-link>
+        <nuxt-link to="/how-to" class="navbar-item">
+          How To
+        </nuxt-link>
+        <nuxt-link to="/about" class="navbar-item">
+          About
+        </nuxt-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isActive: false
+    };
+  }
+};
 </script>
 
 <style></style>
